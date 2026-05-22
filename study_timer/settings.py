a99ee0e42e8ad1d4a147f67ff2f6500e96688caf
nine-boop <jannineprincesslim99@gmail.com>
2026-05-22 14:44:45 +0800

    Update settings.py

--- study_timer/settings.py
+++ study_timer/settings.py
@@ -135,3 +135,12 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
+# --- 💌 REAL EMAIL SMTP CONFIGURATION ---
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# We pull these from Render's secret variables to stay safe!
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '') 
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
